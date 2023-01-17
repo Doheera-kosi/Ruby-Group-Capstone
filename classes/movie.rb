a3e72fc369a6e98369d22252f23b18b86a8192d2
Doheera-kosi <kupourevans99@gmail.com>
2023-01-17 09:43:50 +0000

    Add source.rb & movie.rb

---
+++ classes/movie.rb
@@ -0,0 +1,17 @@
+require_relative './item'
+
+class Movie < Item
+  attr_reader :silent, :publish_date, :id
+
+  def initialize(publish_date, silent: false)
+    super(publish_date)
+    @id = Random.rand(1..1999)
+    @silent = silent
+  end
+
+  private
+
+  def can_be_archived?
+    super || @silent ? true : false
+  end
+end
