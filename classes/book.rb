aa13893e3422355dff7782e645aeec67377c660a
Doheera-kosi <kupourevans99@gmail.com>
2023-01-18 15:31:41 +0000

    Add methods for author.rb, games.rb, item.rb, & book.rb

--- classes/book.rb
+++ classes/book.rb
@@ -1,7 +1,26 @@
+require_relative 'item'
+
 class Book < Item
+  attr_reader :publish_date, :publisher, :cover_state
+
   def initialize(publish_date, publisher, cover_state, id = Random.rand(10_000))
     super(publish_date, false, id)
     @publisher = publisher
     @cover_state = cover_state
+  end
+
+  def can_be_archived?
+    super || cover_state == 'bad'
+  end
+
+  def to_json(*args)
+    {
+      'id' => @id,
+      'archived' => @archived,
+      'cover_state' => @cover_state,
+      'publisher' => @publisher,
+      'publish_date' => @publish_date,
+      'label' => @label
+    }.to_json(*args)
   end
 end
--- classes/games.rb
+++ classes/games.rb
@@ -1,7 +1,29 @@
+require_relative 'item'
+
+# Game class inherits from Item
 class Game < Item
+  attr_reader :multiplayer, :last_played_at
+
   def initialize(publish_date, archived, multiplayer, last_played_at, id = Random.rand(10_000))
     super(publish_date, archived, id)
     @multiplayer = multiplayer
-    @last_played_at = last_played_at
+    @last_played_at = Date.parse(last_played_at)
+  end
+
+  def can_be_archived?
+    super && (Date.today.year - @last_played_at.year) >= 2
+  end
+
+  private :can_be_archived?
+
+  def to_json(*args)
+    {
+      'id' => @id,
+      'publish_date' => @publish_date,
+      'archived' => @archived,
+      'multiplayer' => @multiplayer,
+      'last_played_at' => @last_played_at,
+      'author' => @author
+    }.to_json(*args)
   end
 end
