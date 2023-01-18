b06769ee50700db1e4f363d8afd287909896f3ec
Doheera-kosi <kupourevans99@gmail.com>
2023-01-18 15:34:58 +0000

    Add methods to label.rb, & genre.rb files

--- classes/genre.rb
+++ classes/genre.rb
@@ -1,7 +1,14 @@
 class Genre
+  attr_reader :id, :name, :items
+
   def initialize(name, id = Random.rand(10_000))
     @id = id
     @name = name
     @items = []
+  end
+
+  def add_item(item)
+    @items.push(item)
+    item.genre = self
   end
 end
--- classes/label.rb
+++ classes/label.rb
@@ -1,8 +1,15 @@
 class Label
+  attr_reader :id, :items, :title, :color
+
   def initialize(title, color, id = Random.rand(10_000))
     @id = id
     @items = []
     @title = title
     @color = color
+  end
+
+  def add_item(item)
+    @items << item
+    item.label = self
   end
 end
