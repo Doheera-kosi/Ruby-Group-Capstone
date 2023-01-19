f08ad4d091e005bdd6604f93d6aa4d531770a552
unknown <ekpenisiraphael@gmail.com>
2023-01-19 19:19:55 +0530

    Author storage implemented

--- classes/author.rb
+++ classes/author.rb
@@ -15,4 +15,12 @@ class Author
     @items.push(item) unless @items.include?(item)
     item.author = self
   end
+
+  def to_json(*args)
+    {
+      'id' => @id,
+      'first_name' => @first_name,
+      'last_name' => @last_name
+    }.to_json(*args)
+  end
 end
